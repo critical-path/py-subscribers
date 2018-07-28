34dc431e3cb73083b6d1c15271b284778572a13b
critical-path <37241479+critical-path@users.noreply.github.com>
2018-07-28 15:29:54 -0400

    Update __init__.py

--- subscribers/__init__.py
+++ subscribers/__init__.py
@@ -1,10 +1,5 @@
-"util that retrieves a list of subscribers (watchers) for a given github user and repo."
+"""util that retrieves a list of subscribers (watchers) for a given github user and repo."""
 
 __version__ = "1.0.0"
 
 __author__ = "critical-path"
-
-__all__ = [
-    "subscribers_cli",
-    "subscribers_lib"
-]
